8cc8ef7333a4b18cc5be1601166944f310dee6ba
blurname <naughtybao@outlook.com>
2022-07-09 11:53:02 +0000

    FEAT: ZWSP processor

---
+++ hello-wasm/src/lib.rs
@@ -0,0 +1,15 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    pub fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    alert(&format!("Hello, {}!", name));
+    return;
+}
+#[wasm_bindgen]
+pub fn addZWSP(name: &str) {
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    add_zwsp(String::from("helloworld"));
+}
+pub fn add_zwsp(name: String) {
+    // so many string concatenate ways, which should I choose
+    //
+    // path1 
+    let mut output = String::from("");
+    for ch in name.chars() {
+        output.push(ch);
+        output.push('\u{200b}')
+    }
+
+    // path2
+    let mut output = String::from("");
+    for ch in name.chars() {
+        output.push_str(&(String::from(ch)+ &String::from('\u{200b}')));
+    }
+
+    println!("{}",output)
 }
