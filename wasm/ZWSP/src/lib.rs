e38900d94ead90fe4e81e91d4985944ac4ceeeca
blurname <naughtybao@outlook.com>
2022-07-16 15:08:14 +0800

    ADD: ZWSP finally work

--- wasm/ZWSP/src/lib.rs
+++ wasm/ZWSP/src/lib.rs
@@ -6,22 +6,24 @@ extern "C" {
 }
 
 #[wasm_bindgen]
-pub fn add_zwsp(name: &str) -> i32 {
+pub fn add_zwsp(name: String) -> String {
     // so many string concatenate ways, which should I choose
     //
     // path1
-    alert(&format!("heddllo,{}", 12));
+    // let result = format!("heddllo,{}", name);
+    // alert(&name);
     // let mut output = String::from("");
     // for ch in name.chars() {
     //     output.push(ch);
     //     output.push('\u{200b}')
     // }
 
-    // // path2
-    // let mut output = String::from("");
-    // for ch in name.chars() {
-    //     output.push_str(&(String::from(ch) + &String::from('\u{200b}')));
-    // }
+    // path2
+    let mut output = String::from("");
+    for ch in name.chars() {
+        output.push_str(&(String::from(ch) + &String::from('\u{200b}')));
+    }
     // alert("hello");
-    12
+    // result.into()
+    output
 }
